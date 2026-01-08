fc99bd843032ae90c56de79376336147099d6bf6
Thiago Prazeres <thiagoprazeres@gmail.com>
2026-01-08 01:36:43 -0300

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'suaenergiabrasil-app',
+  appId: 'br.com.suaenergiabrasil',
+  appName: 'Sua Energia Brasil',
   webDir: 'www'
 };
 
